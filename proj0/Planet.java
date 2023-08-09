d1cf56d7d88db6c1182ba349a40d6512d6fbb5f3
2975459755 <135513308+2975459755@users.noreply.github.com>
2023-08-09 20:53:06 +0800

    done proj0

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -3,7 +3,7 @@ public class Planet
     public double xxPos, yyPos, xxVel, yyVel, mass;
     public String imgFileName;
 
-    final static double G = 6.67e-11;
+    private double G = 6.67e-11;    /* the autograder says it ought to be private */
 
     public Planet(double xP, double yP, double xV, double yV, double m, String img)
     {

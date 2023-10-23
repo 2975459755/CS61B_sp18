57b44b8c28388b9d1a2ede4abd72063be5924b91
2975459755 <135513308+2975459755@users.noreply.github.com>
2023-10-23 12:09:39 +0800

    complete optional q

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,5 +1,9 @@
 package lab11.graphs;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -9,6 +13,17 @@ public class MazeAStarPath extends MazeExplorer {
     private boolean targetFound = false;
     private Maze maze;
 
+    private Queue<Integer> queue;
+
+    private class distEstimate<T> implements Comparator<T> {
+        @Override
+        public int compare(Object o1, Object o2) {
+            int v1 = (int) o1;
+            int v2 = (int) o2;
+            return h(v1) - h(v2);
+        }
+    }
+
     public MazeAStarPath(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         maze = m;
@@ -16,11 +31,17 @@ public class MazeAStarPath extends MazeExplorer {
         t = maze.xyTo1D(targetX, targetY);
         distTo[s] = 0;
         edgeTo[s] = s;
+
+        queue = new PriorityQueue<> (new distEstimate<>());
     }
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+        return manhattan(v, t);
+    }
+
+    private int manhattan(int from, int to) {
+        return Math.abs(maze.toX(from) - maze.toX(to)) + Math.abs(maze.toY(from) - maze.toX(to));
     }
 
     /** Finds vertex estimated to be closest to target. */
@@ -30,8 +51,31 @@ public class MazeAStarPath extends MazeExplorer {
     }
 
     /** Performs an A star search from vertex s. */
-    private void astar(int s) {
+    private void astar(int v) {
         // TODO
+        marked[v] = true;
+        announce();
+
+        if (v == t) {
+            targetFound = true;
+        }
+
+        if (targetFound) {
+            return;
+        }
+
+        for (int w: maze.adj(v)) {
+            if (!marked[w]) {
+                queue.add(w);
+                edgeTo[w] = v;
+                marked[w] = true;
+                announce();
+                distTo[w] = distTo[v] + 1;
+            }
+        }
+        while (!queue.isEmpty()) {
+            astar(queue.remove());
+        }
     }
 
     @Override
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,6 +9,7 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    boolean hasCycle = false;
 
     public MazeCycles(Maze m) {
         super(m);
@@ -17,8 +18,30 @@ public class MazeCycles extends MazeExplorer {
     @Override
     public void solve() {
         // TODO: Your code here!
+        distTo[0] = 0;
+        edgeTo[0] = 0;
+        dfs(0);
     }
 
     // Helper methods go here
+    private void dfs(int v) {
+        marked[v] = true;
+        announce();
+
+        for (int w : maze.adj(v)) {
+            if (hasCycle) {
+                return;
+            }
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                announce();
+                distTo[w] = distTo[v] + 1;
+                dfs(w);
+            } else if (w != edgeTo[v]) {
+                hasCycle = true;
+                return;
+            }
+        }
+    }
 }
 

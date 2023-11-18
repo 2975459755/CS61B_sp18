f1a0fdee259455091a8f6f628ebbe0336e9eb42c
2975459755 <135513308+2975459755@users.noreply.github.com>
2023-11-18 20:38:32 +0800

    Update SeamCarver.java

--- hw5/SeamCarver.java
+++ hw5/SeamCarver.java
@@ -3,139 +3,12 @@ import edu.princeton.cs.algs4.Picture;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class SeamCarver {
     Picture pic;
-    Point topLeft;
-    ArrayList<Point> verticalSeam;
-    ArrayList<Point> horizontalSeam;
+    MinPQ<Point> xFirstPQ, yFirstPQ;
 
-    public SeamCarver(Picture picture) {
-        pic = picture;
-        Point[][] matrix = new Point[height()][width()];
-
-        int h = height(), w = width();
-
-        for (int i = 0; i < w; i ++) {
-            for (int j = 0; j < h; j ++) {
-                Point e = new Point(energy(i, j),
-                        get(matrix, i - 1, j), get(matrix, i, j - 1));
-                matrix[j][i] = e;
-            }
-        }
-        topLeft = matrix[0][0];
-    }
-    private static Point get(Point[][] matrix, int x, int y) {
-        try {
-            return matrix[y][x];
-        } catch (Exception exception) {
-            return null;
-        }
-    }
-    private class Point implements Comparable<Point> {
-        Point left, right, up, down;
-        double energy;
-        boolean deleted;
-        Point(double energy, Point left, Point up) {
-            this.energy = energy;
-            this.up = up;
-            this.left = left;
-
-            if (left != null) left.right = this;
-            if (up != null) up.down = this;
-
-            this.deleted = false;
-        }
-        Point findMinVertical() {
-            MinPQ<Point> pq = new MinPQ<>();
-            if (down != null) {
-                pq.insert(down);
-                if (left != null) pq.insert(left.down);
-                if (right != null) pq.insert(right.down);
-            } else {
-                return null;
-            }
-            return pq.delMin();
-        }
-
-        Point findMinHorizontal() {
-            MinPQ<Point> pq = new MinPQ<>();
-            if (right != null) {
-                pq.insert(right);
-                if (up != null) pq.insert(right.up);
-                if (down != null) pq.insert(right.down);
-            } else {
-                return null;
-            }
-            return pq.delMin();
-        }
-
-        int x() {
-            if (left == null) return 0;
-            else return left.x() + 1;
-        }
-        int y() {
-            if (up == null) return 0;
-            else return up.y() + 1;
-        }
-        void calcEnergy() {
-            energy = energy(x(), y());
-        }
-
-        void deleteVerti() {
-            if (this.deleted) return;
-            else this.deleted = true;
-
-            if (left != null)  {
-                left.right = right;
-                left.calcEnergy();
-            }
-            if (right != null) {
-                right.left = left;
-                right.calcEnergy();
-            }
-            if (up != null) {
-                up.down = right;
-                up.calcEnergy();
-            }
-            if (down != null) {
-                down.up = right;
-                down.calcEnergy();
-            }
-        }
-        void deleteHori() {
-            if (this.deleted) return;
-            else this.deleted = true;
-
-            if (up != null) {
-                up.down = down;
-                up.calcEnergy();
-            }
-            if (down != null) {
-                down.up = up;
-                down.calcEnergy();
-            }
-            if (left != null) {
-                left.right = down;
-                left.calcEnergy();
-            }
-            if (right != null) {
-                right.left = down;
-                right.calcEnergy();
-            }
-        }
-        @Override
-        public int compareTo(Point o) {
-            double diff = energy - o.energy;
-            if (diff > 0) {
-                return 1;
-            } else if (diff == 0) {
-                return 0;
-            } else {
-                return -1;
-            }
-        }
-    }
     public Picture picture() {
         return pic;
     }
@@ -161,60 +34,132 @@ public class SeamCarver {
 
         return xGradient + yGradient;
     }
+
+    public SeamCarver(Picture picture) {
+        pic = picture;
+        xFirstPQ = new MinPQ<>(new PointCmp(true? 1 : 0));
+        yFirstPQ = new MinPQ<>(new PointCmp(false? 1 : 0));
+
+        int h = height(), w = width();
+        for (int i = 0; i < w; i ++) {
+            for (int j = 0; j < h; j ++) {
+                Point p = new Point(energy(i, j), i, j);
+                xFirstPQ.insert(p);
+                yFirstPQ.insert(p);
+            }
+        }
+    }
+    private class Point implements Comparable<Point> {
+        double energy;
+        int x, y;
+        Point(double energy, int x, int y) {
+            this.energy = energy;
+            this.x = x;
+            this.y = y;
+
+            /*
+            calc energy -> min energy in seam;
+            access index -> int array in seam;
+            after seam carving:
+                rearrange indices
+                re-calc energy(in four directions)
+             */
+
+        }
+        int index(boolean xFirst) {
+            if (xFirst) return y;
+            else return x;
+        }
+        @Override
+        public int compareTo(Point o) {
+            double diff = energy - o.energy;
+            if (diff > 0) { return 1; }
+            else if (diff == 0) { return 0; }
+            else { return -1; }
+        }
+    }
+    private class PointCmp implements Comparator<Point> {
+        int xFirst;
+        PointCmp(int xFirst) { this.xFirst = xFirst; }
+        @Override
+        public int compare(Point o1, Point o2) {
+            if (xFirst == 1) {
+                if (o1.x != o2.x) return o1.x - o2.x;
+                else return o1.y - o2.y;
+            } else if (xFirst == 0) {
+                if (o1.y != o2.y) return o1.y - o2.y;
+                else return o1.x - o2.x;
+            } else {
+                double diff = o1.energy - o2.energy;
+                if (diff > 0) return 1;
+                else if (diff < 0) return -1;
+                else return 0;
+            }
+        }
+    }
     public int[] findHorizontalSeam() {
-        Point p = topLeft;
-        MinPQ<Point> pq = new MinPQ<> ();
-        while (p != null) {
-            pq.insert(p);
-            p = p.down;
+        int[] ret = new int[width()];
+        ArrayList<Point> seam = new ArrayList<>();
+        MinPQ<Point> firstCol = new MinPQ<>(new PointCmp(2));
+        for (int i = 0; i < height(); i ++) {
+            firstCol.insert(xFirstPQ.delMin());
         }
+        Point first = firstCol.delMin();
+        seam.add(first);
 
-        p = pq.delMin();
-        horizontalSeam = new ArrayList<>();
-        while (p != null) {
-            horizontalSeam.add(p);
-            p = p.findMinHorizontal();
-        }
+        findSeamHelper(seam, first, true, xFirstPQ);
 
-        int[] res = new int[width()];
         for (int i = 0; i < width(); i ++) {
-            res[i] = horizontalSeam.get(i).y();
+            ret[i] = seam.get(i).y;
         }
-
-        return res;
+        return ret;
     }
     public int[] findVerticalSeam() {
-        Point p = topLeft;
-        MinPQ<Point> pq = new MinPQ<> ();
-        while (p != null) {
-            pq.insert(p);
-            p = p.right;
+        int[] ret = new int[height()];
+        ArrayList<Point> seam = new ArrayList<>();
+        MinPQ<Point> firstRow = new MinPQ<>();
+        for (int i = 0; i < width(); i ++) {
+            firstRow.insert(yFirstPQ.delMin());
         }
+        Point first = firstRow.delMin();
+        seam.add(first);
 
-        p = pq.delMin();
-        verticalSeam = new ArrayList<>();
-        while (p != null) {
-            verticalSeam.add(p);
-            p = p.findMinVertical();
-        }
+        findSeamHelper(seam, first, false, yFirstPQ);
 
-        int[] res = new int[height()];
         for (int i = 0; i < height(); i ++) {
-            res[i] = verticalSeam.get(i).x();
+            ret[i] = seam.get(i).x;
         }
+        return ret;
+    }
+    private void findSeamHelper(ArrayList<Point> seam, Point prev, boolean xFirst, MinPQ<Point> points) {
+        int l, n;
+        if (xFirst) {l = height(); n = width();}
+        else {l = width(); n = height();}
 
-        return res;
+        if (seam.size() == n) return;
+
+        MinPQ<Point> pq = new MinPQ<>(new PointCmp(2));
+        ArrayList<Point> list = new ArrayList<>();
+        for (int i = 0; i < l; i ++) {
+            Point p = points.delMin();
+            list.add(p);
+            if (Math.abs(p.index(xFirst) - prev.index(xFirst)) <= 1) {
+                pq.insert(p);
+            }
+        }
+        prev = pq.delMin();
+        seam.add(prev);
+        list.remove(prev);
+        findSeamHelper(seam, prev, xFirst, points);
+
+        for (Point p: list) {
+            points.insert(p);
+        }
     }
     public void removeHorizontalSeam(int[] seam) {
         pic = SeamRemover.removeHorizontalSeam(pic, seam);
-        for (Point p: horizontalSeam) {
-            p.deleteHori();
-        }
     }
     public void removeVerticalSeam(int[] seam) {
         pic = SeamRemover.removeVerticalSeam(pic, seam);
-        for (Point p: verticalSeam) {
-            p.deleteVerti();
-        }
     }
 }
